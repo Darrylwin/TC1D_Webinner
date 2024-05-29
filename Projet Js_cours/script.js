8ac3553086309d32a25e6bb407bb5df41f8f7589
Darrylwin <logossoudarryl20@gmail.com>
2024-05-29 13:16:13 +0000

    exercice de web Ajout des images en fond d'écran

---
+++ Projet Js_cours/script.js
@@ -0,0 +1,19 @@
+// document.body.bgColor = "red";
+function f_red(){
+    document.body.bgColor = "red";
+}
+function f_green(){
+    document.body.bgColor = "green";
+}
+
+function f_color(color){
+    document.body.bgColor = color;
+}
+
+function bg_img1(){
+    document.body.style.backgroundImage = "url('꒰𝙰𝚒 𝙰𝚛𝚝꒱ Madara.jpeg')";
+}
+
+function bg_img2(){
+    document.body.style.backgroundImage = "url('Agatsuma Zenitsu By Muztnafi.jpeg')";
+}
